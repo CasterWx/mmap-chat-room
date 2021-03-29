094ffba15919d403cd62416366050246f3ea5698
AntzUhl <antzuhl1998@gmail.com>
2021-03-29 23:08:45 +0800

    feature(conf/code): add test case

--- src/test/java/com/antzuhl/chat/MappedFileTest.java
+++ src/test/java/com/antzuhl/chat/MappedFileTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-public class MappedFileTest extends ChatRoomApplicationTests{
+public class MappedFileTest {
 
     @Test
     public void mappedFileCreateTest() throws IOException {
-        MappedFile mappedFile = new MappedFile("~/mmap-test", 1024 * 1024 * 10);
+        MappedFile mappedFile = new MappedFile("d:/mmap-test/000000000", 1024 * 1024 * 10);
         assert mappedFile.getWrotePosition() == 0;
         boolean success = mappedFile.appendMessage("mmaptest".getBytes());
         System.out.println(success);
